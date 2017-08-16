c03b11dfc1257274a79c9b997a4f563db2449ff8
Mano Ranjan Jayamaran <jmr12mct@gmail.com>
2017-08-16 16:43:10 +0530

    Add files via upload

---
+++ WeatherStation/src/com/weatherstation/interfaces/DisplayElement.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.weatherstation.interfaces;
+
+/**
+ * @author Mano_Jayamaran
+ *
+ */
+public interface DisplayElement {
+	public void display();
+
+}
---
+++ WeatherStation/src/com/weatherstation/interfaces/Observer.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.weatherstation.interfaces;
+
+/**
+ * @author Mano_Jayamaran
+ *
+ */
+public interface Observer {
+	public void update(float temperature, float humidity, float pressure);
+
+}
